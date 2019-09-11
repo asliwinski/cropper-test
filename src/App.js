c9c72adf51e1d85905b983f3bb6b5726ddbc04dc
asliwinski <andrzej.sliwinski@gmail.com>
2019-09-11 11:15:35 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import CropperWrapper from './CropperWrapper'
+
+class App extends Component {
+  state={
+    viewMode: 0,
+    scale: 1
+  }
+
+  handleChange = event => {
+    this.setState({viewMode: Number(event.target.value)})
+  }
+
+  render() {
+    const {className} = this.props
+
+    return (
+      <div className={className}>
+        <select onChange={this.handleChange} style={{margin: '0 auto'}}>
+          <option value={0}>0</option>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+        </select>
+
+        <CropperWrapper viewMode={this.state.viewMode} scale={this.state.scale}/>
+
+        <input
+          type="range"
+          min="1"
+          max="4"
+          step=".05"
+          value={this.state.scale}
+          onChange={e => {
+            const { value } = e.currentTarget
+            const scale = Number(value)
+            this.setState({ scale })
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+export default styled(App)`
+margin: 0px auto;
+    width: 500px;
+    height: 500px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+`
---
+++ src/CropperWrapper.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import Cropper from 'react-cropper'
+import styled from 'styled-components'
+import 'cropperjs/dist/cropper.css'
+
+const Frame = styled.div`
+  height: 400px;
+  width: 400px;
+  margin: 0 auto;
+  border: 1px solid #979797;
+`
+
+class EditImage extends Component {
+
+  render() {
+    const {className, viewMode, scale} = this.props
+
+    return (
+      <div className={className}>
+        <Frame>
+            <Cropper
+              key={viewMode}
+              ref="cropper"
+              style={{ height: 400, width: 400 }}
+              aspectRatio={1}
+              src={'https://i.imgur.com/fA8tM5R.jpg'}
+              zoomOnWheel={false}
+              scaleX={scale}
+              scaleY={scale}
+              ready={() => {
+                this.refs.cropper.setCropBoxData({
+                  width: 400,
+                  height: 400
+                })
+              }}
+              movable={true}
+              dragMode={'move'}
+              viewMode={viewMode}
+              cropBoxMovable={false}
+              cropBoxResizable={false}
+              background={false}
+              highlight={false}
+              toggleDragModeOnDblclick={false}
+            />
+        </Frame>
+      </div>
+    )
+  }
+}
+
+export default styled(EditImage)`
+  position: relative;
+  width: 500px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
